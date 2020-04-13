e7bfad357c92ac6f404bf9d952ca86463f9d1027
boazrozo <boazrozo@post.bgu.ac.il>
2020-04-13 15:41:41 +0300

    implemeted task 0 of lab 3

---
+++ labs/lab-3/warmup/task-0/hexaPrint/hexaPrint.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void PrintHex(char *buffer, int length) {
+    for (int i = 0; i < length; ++i) {
+        printf("%.2X ", (unsigned char)buffer[i]);
+    }
+}
+
+int main(int argc, char **argv) {
+    char c;
+
+    if (argc != 2) {
+        printf("expected a filename as an argument");
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("an error has occured when opened the file for reading");
+    }
+
+    do {
+        fread(&c, sizeof(char), 1, file);
+        if (feof(file)) {
+            break;
+        }
+
+        if (ferror(file)) {
+            printf("\nan error has occured while reading the file");
+            break;
+        }
+
+        PrintHex(&c, 1);
+    } while (1);
+    printf("\n");
+    return 0;
+}
