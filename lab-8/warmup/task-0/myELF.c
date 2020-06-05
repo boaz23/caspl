fe0a665d042fb9cbd3147ab454afd74fe3bb43ad
boazrozo <boazrozo@post.bgu.ac.il>
2020-06-05 11:19:47 +0300

    Added basic code infastructure for myELF

--- lab-8/warmup/task-0/myELF.c
+++ lab-8/warmup/task-0/myELF.c
@@ -3,6 +3,86 @@
 #include <sys/mman.h>
 #include <elf.h>
 
+#define dbg_out stderr
+
+#define ARR_LEN(a) ((sizeof((a))) / (sizeof(*(a))))
+#define is_str_empty(s) ((s)[0] == '\0')
+
+typedef enum bool bool;
+
+typedef enum bool {
+    FALSE = 0,
+    TRUE  = 1,
+} bool;
+
+int Currentfd = -1;
+void *map_start = NULL;
+bool DebugMode = FALSE;
+
+bool dbg_printf(char const *f, ...) {
+    if (DebugMode) {
+        va_list args;
+        va_start(args, f);
+        vfprintf(dbg_out, f, args);
+        va_end(args);
+        return TRUE;
+    }
+    
+    return FALSE;
+}
+bool dbg_print_error(char const *err) {
+    if (DebugMode) {
+        fprintf(dbg_out, "%s: %s\n", err, strerror(errno));
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+int input_args(int num_args, char const *f, ...) {
+    va_list args;
+    char buffer[256];
+    int scan_result = 0;
+    if (fgets(buffer, ARR_LEN(buffer), stdin) == NULL) {
+        printf("invalid input\n");
+        return 0;
+    }
+    
+    va_start(args, f);
+    scan_result = vsscanf(buffer, f, args);
+    va_end(args);
+    if (scan_result == EOF || scan_result < num_args) {
+        printf("invalid input\n");
+        return 0;
+    }
+    
+    return 1;
+}
+
+int input_int(int *n, char *f) {
+    return input_args(1, f, n);
+}
+int input_int_dec(int *n) {
+    return input_int(n, "%d");
+}
+int input_int_hex(int *n) {
+    return input_int(n, "%X");
+}
+
+int input_filename(char *buffer, int len) {
+    if (fgets(buffer, len, stdin) == NULL) {
+        printf("invalid input\n");
+        return 0;
+    }
+    
+    int input_len = strlen(buffer) - 1;
+    if (buffer[input_len] == '\n') {
+        buffer[input_len] = '\0';
+    }
+
+    return 1;
+}
+
 int main(int argc, char *argv[]) {
     return 0;
 }
